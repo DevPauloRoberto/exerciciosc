f993bbaf840825db96f2c9d033192a81cd02cbf8
unknown <paulean_@hotmail.com>
2025-03-19 20:24:33 -0300

    finalização jogo da velha

--- ex2JogoDaVelha.c
+++ ex2JogoDaVelha.c
@@ -23,49 +23,23 @@ void jogadao(){
     } 
 }
 
+int posicaoDisponivel(int posicao) {
+    if (jogoDaVelha[posicao - 1] == ' ') {
+        return 1; // Posição disponível
+    } else {
+        return 0; // Posição ocupada
+    }
+}
+
 void ganhoux(){
-    if(jogoDaVelha[0] == 'x' && jogoDaVelha[1] == 'x' && jogoDaVelha[2] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[3] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[5] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[6] == 'x' && jogoDaVelha[7] == 'x' && jogoDaVelha[8] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[0] == 'x' && jogoDaVelha[3] == 'x' && jogoDaVelha[6] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[1] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[7] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[2] == 'x' && jogoDaVelha[5] == 'x' && jogoDaVelha[8] == 'x'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[0] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[8] == 'x'){
-        printf("X ganhou\n");
-        win = 2;
-    }
-
-    else if(jogoDaVelha[2] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[6] == 'x'){
+    if( jogoDaVelha[0] == 'x' && jogoDaVelha[1] == 'x' && jogoDaVelha[2] == 'x' ||
+        jogoDaVelha[3] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[5] == 'x' ||
+        jogoDaVelha[6] == 'x' && jogoDaVelha[7] == 'x' && jogoDaVelha[8] == 'x' ||
+        jogoDaVelha[0] == 'x' && jogoDaVelha[3] == 'x' && jogoDaVelha[6] == 'x' ||
+        jogoDaVelha[1] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[7] == 'x' ||
+        jogoDaVelha[2] == 'x' && jogoDaVelha[5] == 'x' && jogoDaVelha[8] == 'x' ||
+        jogoDaVelha[2] == 'x' && jogoDaVelha[4] == 'x' && jogoDaVelha[6] == 'x'
+    ){
         printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
         printf("X ganhou\n");
         win = 2;
@@ -73,127 +47,85 @@ void ganhoux(){
 }
 
 void ganhouo(){
-    if(jogoDaVelha[0] == 'o' && jogoDaVelha[1] == 'o' && jogoDaVelha[2] == 'o'){
+    if( jogoDaVelha[0] == 'o' && jogoDaVelha[1] == 'o' && jogoDaVelha[2] == 'o' ||
+        jogoDaVelha[3] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[5] == 'o' ||
+        jogoDaVelha[6] == 'o' && jogoDaVelha[7] == 'o' && jogoDaVelha[8] == 'o' ||
+        jogoDaVelha[0] == 'o' && jogoDaVelha[3] == 'o' && jogoDaVelha[6] == 'o' ||
+        jogoDaVelha[1] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[7] == 'o' ||
+        jogoDaVelha[2] == 'o' && jogoDaVelha[5] == 'o' && jogoDaVelha[8] == 'o' ||
+        jogoDaVelha[2] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[6] == 'o'
+    ){
         printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
         printf("O ganhou\n");
-    
-        win = 2;
-    }
-
-    else if(jogoDaVelha[3] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[5] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[6] == 'o' && jogoDaVelha[7] == 'o' && jogoDaVelha[8] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[0] == 'o' && jogoDaVelha[3] == 'o' && jogoDaVelha[6] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[1] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[7] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[2] == 'o' && jogoDaVelha[5] == 'o' && jogoDaVelha[8] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[0] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[8] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
-        win = 2;
-    }
-
-    else if(jogoDaVelha[2] == 'o' && jogoDaVelha[4] == 'o' && jogoDaVelha[6] == 'o'){
-        printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\n", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-        printf("O ganhou\n");
-        
         win = 2;
     }
 }
 
 int main() {
+    inicio_jogo:
 
-    while(win == 1){
-
-        if(jogoDaVelha[0] != ' ' && jogoDaVelha[1] != ' ' && jogoDaVelha[2] != ' ' && jogoDaVelha[3] != ' ' && jogoDaVelha[4] != ' ' && jogoDaVelha[5] != ' ' && jogoDaVelha[6] != ' ' && jogoDaVelha[7] != ' ' && jogoDaVelha[8] != ' '){
-            printf("Empate");
-            return 0;
-        }
-
-        while(1){
-
-            printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\nselecione a posicao que voce quer jogar", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-            resultado = scanf("%d", &position);
-            if(resultado == 1){
-                if(position > 9 || position <= 0){
-                    printf("Somente valores 1 a 9\n");
-                    printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\nselecione a posicao que voce quer jogar", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-                    scanf("%d", &position);
-                    while(getchar() != '\n');
-                }
-                else{
-                    break;
-                }
-            }
-            else{
-                printf("somente numeros\n");
-                while(getchar() != '\n'); // Professor após pesquisa, e muitos loops infinitos, descobri a existencia do "buffer", e que caso ele receba um caracter inválido ele fica lá e preciso limpa-lo senão o sistema entra em looping de erro, aprendi que com o while(getchar() != '\n')  vai lendo 1 caracter errado por vez e como não está em uma variável ele não salva em nada e ele some, até encontrar a quebra de linha que é o enter do usuário, sabendo assim que o buffer está limpo
-            }
-        }
-        jogadax();
-        ganhoux();
-        ganhouo();
-
-        if(win == 2){
-            return 0;
-        }
-
-        if(jogoDaVelha[0] != ' ' && jogoDaVelha[1] != ' ' && jogoDaVelha[2] != ' ' && jogoDaVelha[3] != ' ' && jogoDaVelha[4] != ' ' && jogoDaVelha[5] != ' ' && jogoDaVelha[6] != ' ' && jogoDaVelha[7] != ' ' && jogoDaVelha[8] != ' '){
-            printf("Empate");
-            return 0;
-        }
-
-        while(1){
-            printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\nselecione a posicao que voce quer jogar", jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
-
-            resultado = scanf("%d", &position);
-            if(resultado == 1){
-                if(position > 9 || position <= 0){
-                    printf("Somente valores 1 a 9\n");
-                    scanf("%d", &position);
-                    while(getchar() != '\n');
-                }
-                else{
-                    break;
-                }
-            }
-            else{
-                printf("somente numeros\n");
-                while(getchar() != '\n'); // Professor após pesquisa, e muitos loops infinitos, descobri a existencia do "buffer", e que caso ele receba um caracter inválido ele fica lá e preciso limpa-lo senão o sistema entra em looping de erro, aprendi que com o while(getchar() != '\n')  vai lendo 1 caracter errado por vez e como não está em uma variável ele não salva em nada e ele some, até encontrar a quebra de linha que é o enter do usuário, sabendo assim que o buffer está limpo
-            }
-        }
-        jogadao();
-        ganhoux();
-        ganhouo();
+    if (jogoDaVelha[0] != ' ' && jogoDaVelha[1] != ' ' && jogoDaVelha[2] != ' ' &&
+        jogoDaVelha[3] != ' ' && jogoDaVelha[4] != ' ' && jogoDaVelha[5] != ' ' &&
+        jogoDaVelha[6] != ' ' && jogoDaVelha[7] != ' ' && jogoDaVelha[8] != ' ') {
+        printf("Empate\n");
+        return 0;
     }
-    return 0;
+
+    // jogada x
+    jogador_x:
+    printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\nselecione a posicao que voce quer jogar", 
+           jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], 
+           jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], 
+           jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
+    resultado = scanf("%d", &position);
+    if (resultado == 1) {
+        if (position > 9 || position <= 0) {
+            printf("Somente valores 1 a 9\n");
+            goto jogador_x;
+        } else if (jogoDaVelha[position - 1] != ' ') { // Verifica se a posição está ocupada
+            printf("Posicao ja ocupada. Escolha outra.\n");
+            goto jogador_x;
+        }
+    } else {
+        printf("Somente numeros\n");
+        while (getchar() != '\n'); // Limpa o buffer
+        goto jogador_x;
+    }
+
+    jogadax();
+    ganhoux();
+
+    if (win == 2) {
+        return 0;
+    }
+
+    // jogada o
+    jogador_o:
+    printf(" %c|%c|%c\n-------\n %c|%c|%c\n-------\n %c|%c|%c\n\nselecione a posicao que voce quer jogar", 
+           jogoDaVelha[0], jogoDaVelha[1], jogoDaVelha[2], 
+           jogoDaVelha[3], jogoDaVelha[4], jogoDaVelha[5], 
+           jogoDaVelha[6], jogoDaVelha[7], jogoDaVelha[8]);
+    resultado = scanf("%d", &position);
+    if (resultado == 1) {
+        if (position > 9 || position <= 0) {
+            printf("Somente valores 1 a 9\n");
+            goto jogador_o;
+        } else if (jogoDaVelha[position - 1] != ' ') { // Verifica se a posição está ocupada
+            printf("Posicao ja ocupada. Escolha outra.\n");
+            goto jogador_o;
+        }
+    } else {
+        printf("Somente numeros\n");
+        while (getchar() != '\n'); // Limpa o buffer
+        goto jogador_o;
+    }
+
+    jogadao();
+    ganhouo();
+
+    if (win == 2) {
+        return 0;
+    }
+
+    goto inicio_jogo;
 }
